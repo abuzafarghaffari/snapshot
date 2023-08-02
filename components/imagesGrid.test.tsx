a55b01daf9fb595721815e22f0e69e20bb0868bc
abuzafar <abuzafar.ghaffari@kellton.com>
2023-08-02 11:15:00 +0530

    snapshot

--- components/imagesGrid.test.tsx
+++ components/imagesGrid.test.tsx
@@ -10,7 +10,7 @@ jest.mock("../helper/api-util.js", () => ({
       {
         id:"1",
         cc: "28 jan",
-        title: "Himalayas",
+        title: "mountain1",
         category: "Mountain",
         url:"https://images.unsplash.com/photo-1512250591270-0bea37004c99?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
         
@@ -18,8 +18,8 @@ jest.mock("../helper/api-util.js", () => ({
       {
         id:"2",
         timeStamp: "64c27168e1feb5ce8f572abb",
-        title: "Anjuna Beach",
-        category: "Beaches",
+        title: "bird1",
+        category: "bird",
         imageUrl:"https://media.istockphoto.com/id/936604286/photo/holes-at-the-biches-coastline-mauritius-island.jpg?s=612x612&w=0&k=20&c=pvT7qEVmG_aBkQpYLGvDe_tdc9SkwyKVHUlYwEGfNgw=",
         
       },
@@ -39,9 +39,9 @@ const setImageHandler = (img: string) => {
     render(<ImagesGridLists gallaryItems={data} setImage={setImageHandler}/>);
   
     // Use screen.getByText to assert that each item's title is present in the rendered component
-    expect(screen.getByText("Title: Anjuna Beach")).toBeInTheDocument();
+    expect(screen.getByText("Title: bird1")).toBeInTheDocument();
     //expect(screen.getByText("Category: Beach")).toBeInTheDocument();
-    expect(screen.getByText("Title: Himalayas")).toBeInTheDocument();
+    expect(screen.getByText("Title: mountain1")).toBeInTheDocument();
   });
 
   test('should show "No Record found" when no matching items are found', async () => {
